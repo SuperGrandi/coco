7288f8df02055f87a72a14320dfca1fd961471e5
hand <tkdwns0301@gmail.com>
2021-04-05 20:24:26 +0900

    [New] Activity and screen
    2021.04.05

---
+++ app/src/main/java/com/example/coco/lock_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.coco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lock_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lock_screen)
+    }
+}
