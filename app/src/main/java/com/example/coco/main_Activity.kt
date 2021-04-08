575bda45317ee47d010a076c71fa1bfeb61c3291
SiyeolBaek <qortlduf505@gmail.com>
2021-04-09 03:52:37 +0900

    [Revert] Main, Chat Activity

--- app/src/main/java/com/example/coco/main_Activity.kt
+++ app/src/main/java/com/example/coco/main_Activity.kt
@@ -1,11 +1,20 @@
 package com.example.coco
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class main_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen)
+
+        val btn_event = findViewById<Button>(R.id.moveChat)
+
+        btn_event.setOnClickListener {
+            val chatPage = Intent(this, ChatActivity::class.java)
+            startActivity(chatPage)
+        }
     }
 }
